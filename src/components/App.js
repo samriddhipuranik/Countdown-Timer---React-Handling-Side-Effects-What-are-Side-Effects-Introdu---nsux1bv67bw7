fb0173dd2a748193a321ac5ef80a1762927fe18c
Samriddhi Puranik <samriddhi.18puranik@gmail.com>
2023-02-07 20:47:01 +0530

    integer update!

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,26 @@ import '../styles/App.css';
 const App = () => {
   // write your code here 
 const [counter, setCounter]= useState(0);
+let decrementCount =()=>{
+  <label>Enter the countdown integer:
+
+    <input type={Number} value={inputs.counter}></input>
+  </label>
+  setCounter(counter-1);
+};
+
+useEffect(() => {
+  clearTimer(getDeadTime());
+}, []);
+
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={setCounter(counter-1)} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={decrementCount}  /> sec.
         </h1>
       </div>
-      <div id="current-time">{setCounter(counter)}</div>
+      <div id="current-time">0</div>
     </div>
   )
 }
